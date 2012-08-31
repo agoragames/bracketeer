b690d5162ea21443f02a99b37b228000fc68e30e
Andrew Nordman <cadwallion@gmail.com>
2012-08-31 16:45:41 -0500

    Actually generate JSON, silly

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -6,7 +6,7 @@ class BracketsController < ApplicationController
   def export
     title = params[:title].parameterize
     json = JSON.parse(params[:bracket])
-    send_data json, type: 'application/vnd.bracket_tree',
+    send_data JSON.generate(json), type: 'application/vnd.bracket_tree',
       filename: "#{title}.json", status: 200
   end
 end
